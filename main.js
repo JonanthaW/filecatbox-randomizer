ca42f1bdd2e866fd1ad545752526863596db65df
JonanthaW <jonanthawillian@gmail.com>
2023-11-22 00:53:38 -0300

    Added new version using python && added Not found URLS

---
+++ main.js
@@ -0,0 +1,130 @@
+const { Builder, By } = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+const fs = require('fs').promises;
+
+// Get the number of drivers from the user (replace 2 with the desired number)
+const numDrivers = 20;
+
+// Constants
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const MAX_URL_LENGTH = 6;
+const BASE_URL = 'https://files.catbox.moe/';
+
+// File paths
+const notFoundURLsFilePath = 'notFoundURLs.txt';
+const workingURLsFilePath = 'workingURLs.txt';
+
+// Function to initialize WebDrivers
+async function initializeWebDrivers(numDrivers) {
+    const drivers = [];
+
+    try {
+        let options = new firefox.Options();
+
+        for (let i = 0; i < numDrivers; i++) {
+            const driver = await new Builder()
+                .forBrowser('firefox')
+                .setFirefoxOptions(options.addArguments('--headless'))
+                .build();
+            drivers.push(driver);
+        }
+
+        console.log(`${numDrivers} WebDrivers initialized successfully.`);
+
+        // Run tryLink for all tabs concurrently
+        await Promise.all(drivers.map((driver, index) => tryLink(driver, index + 1)));
+    } catch (err) {
+        console.error('Error initializing WebDrivers:', err);
+    } finally {
+        // Close all drivers when done
+        await Promise.all(drivers.map(driver => driver.quit()));
+    }
+}
+
+// Main function to attempt accessing the link
+async function tryLink(driver, tabNumber) {
+    let url = generateRandomURL();
+
+    // Check if the URL has already been tested and found not found
+    if (await isUrlNotFound(url)) {
+        console.log(`Tab ${tabNumber}: URL already tested and not found: ${url}`);
+        return;
+    }
+
+    try {
+        await driver.get(`${BASE_URL}${url}.mp4`);
+        const element = await driver.findElement(By.tagName('html'));
+        const text = await element.getText();
+
+        if (text === "404! not found!") {
+            console.log(`Tab ${tabNumber}: Link not found for URL: ${url}`);
+            await addToNotFoundURLs(url); // Add the URL to the notFoundURLs file
+            await driver.sleep(50);
+            await tryLink(driver, tabNumber);
+        } else {
+            console.log(`Tab ${tabNumber}: Link found! URL: ${BASE_URL}${url}.mp4`);
+            await addToWorkingURLs(url); // Add the URL to the workingURLs file
+            await tryLink(driver, tabNumber);
+        }
+    } catch (error) {
+        console.error(`Tab ${tabNumber}: An error occurred:`, error);
+    }
+}
+
+// Function to generate a random URL
+function generateRandomURL() {
+    let url = "";
+    for (let i = 0; i < MAX_URL_LENGTH; i++) {
+        url += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+    }
+    return url;
+}
+
+// Function to check if the URL is in the notFoundURLs file
+async function isUrlNotFound(url) {
+    try {
+        const notFoundURLs = await getNotFoundURLs();
+        return notFoundURLs.includes(url);
+    } catch (error) {
+        console.error('Error checking if URL is not found:', error);
+        return false;
+    }
+}
+
+// Function to get the content of the notFoundURLs file
+async function getNotFoundURLs() {
+    try {
+        const content = await fs.readFile(notFoundURLsFilePath, 'utf-8');
+        return content.trim().split('\n');
+    } catch (error) {
+        // If the file doesn't exist, return an empty array
+        if (error.code === 'ENOENT') {
+            return [];
+        }
+        console.error('Error reading notFoundURLs file:', error);
+        return [];
+    }
+}
+
+// Function to add the URL to the notFoundURLs file
+async function addToNotFoundURLs(url) {
+    try {
+        await fs.appendFile(notFoundURLsFilePath, url + '\n');
+    } catch (error) {
+        console.error('Error adding URL to notFoundURLs file:', error);
+    }
+}
+
+// Function to add the URL to the workingURLs file
+async function addToWorkingURLs(url) {
+    try {
+        await fs.appendFile(workingURLsFilePath, url + '\n');
+    } catch (error) {
+        console.error('Error adding URL to workingURLs file:', error);
+    }
+}
+
+// Execute the initialization
+(async function () {
+    await initializeWebDrivers(numDrivers);
+})();
---
+++ main.py
@@ -0,0 +1,103 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import random
+import string
+
+# Constants
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+MAX_URL_LENGTH = 6
+BASE_URL = 'https://files.catbox.moe/'
+
+# File paths
+not_found_urls_file_path = 'notFoundURLs.txt'
+working_urls_file_path = 'workingURLs.txt'
+
+# Function to initialize WebDriver
+def initialize_webdriver():
+    try:
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Firefox(options=options)
+        print("WebDriver initialized successfully.")
+
+        # Run try_link
+        try_link(driver)
+
+    except Exception as e:
+        print('Error initializing WebDriver:', e)
+    finally:
+        # Close the driver when done
+        driver.quit()
+
+# Main function to attempt accessing the link
+def try_link(driver):
+    url = generate_random_url()
+
+    # Check if the URL has already been tested and found not found
+    if is_url_not_found(url):
+        print(f"URL already tested and not found: {url}")
+        return
+
+    try:
+        driver.get(f"{BASE_URL}{url}.mp4")
+        element = driver.find_element(By.CSS_SELECTOR, "body > img:nth-child(1)")
+        src = element.get_attribute('src')
+
+        if src == "https://files.catbox.moe/official/images/404.png":
+            print(f"Link not found for URL: {url}")
+            add_to_not_found_urls(url)  # Add the URL to the notFoundURLs file
+            time.sleep(0.05)
+            try_link(driver)
+        else:
+            print(f"Link found! URL: {BASE_URL}{url}.mp4")
+            add_to_working_urls(url)  # Add the URL to the workingURLs file
+            time.sleep(0.05)
+            try_link(driver)
+    except Exception as error:
+        print("An error occurred:", error)
+
+# Function to generate a random URL
+def generate_random_url():
+    return ''.join(random.choice(ALPHABET) for _ in range(MAX_URL_LENGTH))
+
+# Function to check if the URL is in the not_found_urls file
+def is_url_not_found(url):
+    try:
+        not_found_urls = get_not_found_urls()
+        return url in not_found_urls
+    except Exception as error:
+        print('Error checking if URL is not found:', error)
+        return False
+
+# Function to get the content of the not_found_urls file
+def get_not_found_urls():
+    try:
+        with open(not_found_urls_file_path, 'r') as file:
+            content = file.read()
+            return content.strip().split('\n')
+    except FileNotFoundError:
+        return []
+    except Exception as error:
+        print('Error reading not_found_urls file:', error)
+        return []
+
+# Function to add the URL to the not_found_urls file
+def add_to_not_found_urls(url):
+    try:
+        with open(not_found_urls_file_path, 'a') as file:
+            file.write(url + '\n')
+    except Exception as error:
+        print('Error adding URL to not_found_urls file:', error)
+
+# Function to add the URL to the working_urls file
+def add_to_working_urls(url):
+    try:
+        with open(working_urls_file_path, 'a') as file:
+            file.write(url + '\n')
+    except Exception as error:
+        print('Error adding URL to working_urls file:', error)
+
+# Execute the initialization
+if __name__ == "__main__":
+    initialize_webdriver()
